422c4c24b1ddf2c4d6226ca55a80a568d29b005c
Gaston Irby III <gaston@Gastons-iMac.hsd1.wa.comcast.net>
2022-10-19 15:27:59 -0700

    set state

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const getPlants = () => {
+    return <ActivityIndicator size='large' />;
+  };
+
   return (
     <View style={styles.container}>
+      {getPlants()}
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
